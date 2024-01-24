c85f09a52d89a34a0deef53b9187396e4531277f
lihen12 <henryli0@yahoo.com>
2024-01-23 20:31:31 -0800

    Making changes to deploy on cloud

--- api/src/config/database.js
+++ api/src/config/database.js
@@ -1,10 +1,14 @@
-require('dotenv').config();
+//require('dotenv').config();
+const path = require('path');// new
+
+require('dotenv').config({ path: path.join(__dirname, '../../../.env') });
 
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL || 'postgresql://postgres:chicagobulls1@localhost:5432/tft_stats',
-    ssl: process.env.DATABASE_URL ? true : false
+    connectionString: process.env.DATABASE_URL /*|| 'postgresql://postgres:chicagobulls1@localhost:5432/tft_stats'*/,
+    //ssl: process.env.DATABASE_URL ? true : false
+    ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
 });
 
 module.exports = pool;
